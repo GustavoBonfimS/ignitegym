3ddecd65ee2a52905e307af84a03b090f759ff0e
Gustavo Bonfim <gustavo.hbonfim@outlook.com>
2023-01-14 18:45:30 -0300

    app assets

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,6 +12,11 @@ export default function App() {
   });
   return (
     <View>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? (
         <Text>Open up App.js to start working on your app!</Text>
       ) : (
